c95e71e1524fee6c188dc869efcd40ff697a4a57
Dyfan Jones <dyfanjones@hotmail.com>
2017-06-17 11:49:51 +0100

    fixed bugs

--- R/dbtbls.R
+++ R/dbtbls.R
@@ -37,22 +37,16 @@ dbtbls<-function(conn, db=NULL, schema=NULL){
   
   if(conn$info$dbms.name=="PostgreSQL") {
     
-    db<-dbase(conn,db)
     schema<-schem(schema)
     
-    sel<-"SELECT a.relname as Table, pg_size_pretty(pg_total_relation_size(relid)) As Size, pg_size_pretty(pg_total_relation_size(relid) - pg_relation_size(relid)) as External_Size, b.tableowner"
-    fro<-" FROM pg_catalog.pg_statio_user_tables as a inner join pg_tables as b on a.relname = b.tablename"
-    wh<-paste0(" where a.schemaname IN (\'",schema,"\')")
+    sel<-"SELECT Schemaname, Tablename, tableowner"
+    fro<-" FROM pg_catalog.pg_tables"
+    wh<-paste0(" where schemaname IN (\'",schema,"\')")
     
     query<-paste0(sel,fro,wh)
     
-    tbl(conn,sql(query))->result
     as.data.frame(tbl(conn,sql(query)))->result
-    dplyr::mutate(result,type = substr(size,nchar(size)-2,nchar(size)),
-                  N = ifelse(type==tolower("kb"),as.numeric(substr(size, 1, nchar(size)-3)),
-                             ifelse(type==tolower("mb"),as.numeric(substr(size, 1, nchar(size)-3))*0.001,
-                                    ifelse(type==tolower("gb"),as.numeric(substr(size, 1, nchar(size)-3))*0.00001,1))))->result
-    result<-arrange(result, desc(N)) %>% select(-N,-type)
+    result<-arrange(result, tableowner)
   }
   return(result)
 }
